acdb0227eecda9f0094943db8c8be3cfa5adae2d
Brandon Brandhuber <brandon.brandhuber@stu.ocsb.ca>
2025-04-10 13:55:38 +0000

    Volume of Triangular Pyramid

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 BrandonBCode All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: BrandonBCode
+// Created on: March 2025
 // This file contains the JS functions for index.html
+
+"use strict";
+/**
+ * This function calculates the area of the triangular pyramid
+ */
+function calculateArea() {
+  // input that takes side a, b and height of the triangular pyramid from the user and converts it.
+  let baseA = parseFloat(document.getElementById('base').value);
+  let sideA = parseFloat(document.getElementById('side-1').value);
+  let sideH = parseFloat(document.getElementById('height').value);
+  
+
+  //calculates area of triangular pyramid
+    let areaofTriPyramid = (1/6)* baseA * sideA * sideH;
+
+  // output that displays the area of triangular pyramid
+    document.getElementById("area-tripyramid").innerHTML = "The area of the triangular pyramid is: " + areaofTriPyramid.toFixed(1) + " cm²";
+
+}
